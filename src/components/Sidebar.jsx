b34649f7b4176d805bff9674b43bbd5eb6f2458b
alekanDev <alekandev@gmail.com>
2022-08-31 21:51:16 -0500

    Add react-icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import '../styles/sidebar.css'
+import { FaCity, FaHome, FaSimplybuilt } from "react-icons/fa"
+
+
 
 const Sidebar = () => {
   return (
-    <div>
-        
+    <div className='sidebarContent'>
+        <ul className='itemsMenu'>
+          <li> <FaHome /> <span>Home</span></li>
+          <li> <FaCity/> <span>Companies</span></li>
+          <li> <FaSimplybuilt /> <span>Devices</span></li>
+          
+        </ul>
     </div>
   )
 }
---
+++ src/layouts/PrivateLayout.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../styles/privateLayout.css'
+import logo from '../images/logo.png'
+
+// Components
+import Sidebar from '../components/Sidebar.jsx'
+import Navbar from '../components/Navbar.jsx'
+import Footer from '../components/Footer.jsx'
+
+const Admin = ({ Children }) => {
+  return (
+    <div className='privateLayout'>
+      <div className='leftPrivate'>
+        <div className="logo">
+          <img src={ logo } alt="" />
+        </div>
+        <div className="sideBar">
+      <Sidebar />
+        </div>
+      </div>
+      <div className='rightPrivate'>
+        <div className='navbarPrivate'>
+          <Navbar />
+        </div>
+          <main>
+            {Children}
+          </main>
+        <div className='footerPrivate'>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Admin
