26cd61d2a9120e49e8458d32adc06068bbe4369a
alekanDev <alekandev@gmail.com>
2022-09-02 15:38:23 -0500

    navbar, logout and search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import '../styles/navbar.css'
+import { FaSearch, FaPowerOff } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>
-        navbarPage
+    <div className='navbarContent centerVer'>
+      <div className='searchContent centerVer'>
+        <div className="search centerVer">
+          <form className='searchForm' action="" method="get">
+            <FaSearch color={'#EA094B'} size={ 20 } />
+            <input type="text" placeholder='Search'/>
+          </form>
+        </div>
+      </div>
+      <div className='loginInfo centerVer'>
+        <div className="logout">
+          <FaPowerOff color='#EA094B' size={ 20 } />
+        </div>
+      </div>
     </div>
   )
 }
